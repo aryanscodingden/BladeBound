e0db60af03cdc8bbd91d9c96e7ee0a62b519d379
Aryan Rao <aryanscode@gmail.com>
2026-01-12 23:12:02 +0530

    Added basic player motion

--- player/player.gd
+++ player/player.gd
@@ -1,1 +1,19 @@
-extends Sprite2D
+extends CharacterBody2D
+
+const speed = 100.0 
+
+func _physics_process(_delta: float) -> void:
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x = speed
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -speed
+	if Input.is_action_pressed("move_up"):
+		velocity.y = -speed
+	if Input.is_action_pressed("move_down"):
+		velocity.y = speed
+	if Input.is_action_pressed("move_right"):
+		print("Right pressed")
+	move_and_slide()
+	
+	

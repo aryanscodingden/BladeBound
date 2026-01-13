91a0ee41eaaa32892063633f5314f1edcf99313a
Aryan Rao <aryanscode@gmail.com>
2026-01-14 00:14:33 +0530

    animationtree handler to switch bw animations smoothly

--- player/player.gd
+++ player/player.gd
@@ -2,9 +2,11 @@ extends CharacterBody2D
 
 const speed = 100.0 
 
+var input_vector = Vector2.ZERO
+
 func _physics_process(_delta: float) -> void:
 	
-	var input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
 	velocity = input_vector * speed
 	move_and_slide()

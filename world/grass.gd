8700ad6adf096edd734406707fd1f87aca9da980
Aryan Rao <aryanscode@gmail.com>
2026-01-18 20:20:37 +0530

    more bat enemy script

--- world/grass.gd
+++ world/grass.gd
@@ -1,7 +1,6 @@
 extends Node2D
-const grass_effect = preload("res://effects/grass_effect.tscn")
 @onready var area_2d: Area2D = $Area2D
-
+@export var grass_effect: PackedScene
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	area_2d.area_entered.connect(_on_area_2d_area_entered)

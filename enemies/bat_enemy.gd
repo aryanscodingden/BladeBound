a69f4e0e67a3fca5d4e8f49ad0536710231c206e
Aryan Rao <aryanscode@gmail.com>
2026-01-19 23:27:39 +0530

    Bug fix: Bat snapping to player

--- enemies/bat_enemy.gd
+++ enemies/bat_enemy.gd
@@ -1,14 +1,11 @@
 extends CharacterBody2D
-const speed = 30
-
 @export var RangePlayer:= 104
-
+const speed = 30 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var animation_tree: AnimationTree = $AnimationTree
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
 @onready var playback = animation_tree.get("parameters/StateMachine/playback") as AnimationNodeStateMachinePlayback
 @export var player: Player
-@onready var ray_cast_2d: RayCast2D = $RayCast2D
-
 
 func _physics_process(_delta: float) -> void:
 	var state = playback.get_current_node()
@@ -18,30 +15,33 @@ func _physics_process(_delta: float) -> void:
 			player = get_player()
 			if player is Player:
 				velocity = global_position.direction_to(player.global_position) * speed
-				sprite_2d.scale.x = sign(velocity.x)
 			else:
 				velocity = Vector2.ZERO
+				#sprite_2d.scale.x = sign(velocity.x)
 			move_and_slide()
-
+			
 func get_player() -> Player:
 	return get_tree().get_first_node_in_group("player")
-
+	
 func is_player_in_range() -> bool:
-	var result = false 
+	var result = false
 	player = get_player()
 	if player is Player:
 		var distance_to_player = global_position.distance_to(player.global_position)
-		if distance_to_player < RangePlayer: 
+		if distance_to_player < RangePlayer:
 			result = true
-	return result 
-
+	return result
+	
 func can_see_player() -> bool:
 	if not is_player_in_range(): return false
-	player = get_player()
+	var player: = get_player()
 	ray_cast_2d.target_position = player.global_position - global_position
-	print("raycast")
 	ray_cast_2d.force_raycast_update()
 	var has_los_to_player: = not ray_cast_2d.is_colliding()
 	return has_los_to_player
+	
+	
+	
+	
 
 	

5a434760deb2874943794dc9a4c0e428489dd18f
Aryan Rao <aryanscode@gmail.com>
2026-01-24 23:36:17 +0530

    New coin system - only small outlines

---
+++ GameManager/game_manager.gd
@@ -0,0 +1,13 @@
+extends Node
+
+var coins: int = 0 
+var kills: int = 0 
+var grass_cut: int = 0
+
+signal coins_changed(amount)
+
+func add_coins(amount: int):
+	coins += amount
+	coins_changed.emit(coins)
+	print("You've got coins, Total:", [amount, coins])
+	

f47cfa9ac03eb1f25182529d8689d4a071140a13
Aryan Rao <aryanscode@gmail.com>
2026-01-25 19:20:13 +0530

    Added the coin HUD to the world scene, bug fix: global posistion find

--- environment/Environmentgrass.gd
+++ environment/Environmentgrass.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
 @onready var sprite: Sprite2D = $Sprite2D
+
 var is_cut: bool = false
-const COIN_SCENE = preload("uid://lx6172aku7o7")
+
+# Preload the coin scene (correct UID for effects/coin.tscn)
+const COIN_SCENE = preload("uid://c6yxctc7uh8ne")
 
 func _ready():
 	add_to_group("cuttable_grass")
@@ -13,20 +16,28 @@ func cut_grass():
 	is_cut = true
 	print("Grass was cut")
 	
+	var spawn_position = global_position
 	var tween = create_tween()
 	tween.tween_property(sprite, "modulate:a", 0.0, 0.3)
 	tween.tween_property(sprite, "scale", Vector2(1.2, 0.8), 0.3)
-	
 	var coin_amount = randi_range(1, 3)
-	spawn_coins(coin_amount)
+	spawn_coins(coin_amount, spawn_position)
 	GameManager.grass_cut += 1
-	
+   
 	await tween.finished
 	queue_free()
 
-func spawn_coins(amount: int):
+func spawn_coins(amount: int, spawn_pos: Vector2):
 	for i in amount:
 		var coin = COIN_SCENE.instantiate()
-		get_tree().current_scene.add_child(coin)
-		var offset = Vector2(randf_range(-15, 15), randf_range(-15, 15))
-		coin.global_position = global_position + offset
+		if coin == null:
+			print("❌ ERROR: Could not create coin!")
+			return
+		var world = get_parent()
+		if world:
+			world.add_child(coin)
+			var offset = Vector2(randf_range(-15, 15), randf_range(-15, 15))
+			coin.global_position = spawn_pos + offset
+			print("Coin spawned at: %s" % coin.global_position)
+		else:
+			print("ERROR: Could not find world!")
--- ui/hud.gd
+++ ui/hud.gd
@@ -2,19 +2,24 @@ extends CanvasLayer
 @onready var coin_amount_label: Label = $CoinDisplay/CoinAmountLabel
 
 func _ready() -> void:
-	GameManager.coins_changed.connect(update_coin_display)
+	setup_coin_display()
+func setup_coin_display():
+	if has_node("CoinDisplay/CoinAmountLabel"):
+		coin_amount_label = get_node("CoinDisplay/CoinAmountLabel")
+	elif has_node("CoinAmountLabel"):
+		coin_amount_label = get_node("CoinAmountLabel")
+	else:
+		for child in get_children():
+			return 
+			
+	if GameManager.coins_changed.is_connected(update_coin_display):
+		print("Already connected to coins_changed")
+	else:
+		GameManager.coins_changed.connect(update_coin_display)
+		
 	update_coin_display(GameManager.coins)
 	print("Coin display ready!")
-	if coin_amount_label:
-		GameManager.coins_changed.connect(update_coin_display)
-		update_coin_display(GameManager.coins)
-		print("Coin display ready")
-	else:
-		print("Cant find CoinAmountLabel")
-		
-	
-func update_coin_display(amount:int):
+
+func update_coin_display(amount: int):
 	if coin_amount_label:
 		coin_amount_label.text = str(amount)
-		print("Coin amount displayed")
-		

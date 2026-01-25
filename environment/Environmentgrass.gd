bdfebb6dfac953c904ab283496eb1ca70e9d73d4
Aryan Rao <aryanscode@gmail.com>
2026-01-25 22:45:23 +0530

    Coin spawn chances on cutting bushes lowered

--- environment/Environmentgrass.gd
+++ environment/Environmentgrass.gd
@@ -3,8 +3,6 @@ extends Node2D
 @onready var sprite: Sprite2D = $Sprite2D
 
 var is_cut: bool = false
-
-# Preload the coin scene (correct UID for effects/coin.tscn)
 const COIN_SCENE = preload("uid://c6yxctc7uh8ne")
 
 func _ready():
@@ -20,7 +18,13 @@ func cut_grass():
 	var tween = create_tween()
 	tween.tween_property(sprite, "modulate:a", 0.0, 0.3)
 	tween.tween_property(sprite, "scale", Vector2(1.2, 0.8), 0.3)
-	var coin_amount = randi_range(1, 3)
+	var coin_amount = 0
+	if randf() > 0.7:
+		coin_amount = randi_range(1,2)
+	if coin_amount > 0:
+		spawn_coins(coin_amount, spawn_position)
+	else: 
+		print("no coins from this coins for you ha")
 	spawn_coins(coin_amount, spawn_position)
 	GameManager.grass_cut += 1
    
@@ -31,7 +35,6 @@ func spawn_coins(amount: int, spawn_pos: Vector2):
 	for i in amount:
 		var coin = COIN_SCENE.instantiate()
 		if coin == null:
-			print("❌ ERROR: Could not create coin!")
 			return
 		var world = get_parent()
 		if world:
